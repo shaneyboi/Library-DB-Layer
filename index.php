d61aab0f836632d9a7bf916ea4cdc029a2da2632
Shane Avery Sistoza <ss2010@hawaii.edu>
2016-11-02 13:18:45 -1000

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+use UHHiloLibrary\Voyager\Voyager as Voyager;
+use UHHiloLibrary\Voyager\Status as Status;
+use UHHiloLibrary\Voyager\StudyRoom as StudyRoom;
+use UHHiloLibrary\Circulation\InternalCount as InternalCount;
+use UHHiloLibrary\Circulation\Information as Information;
+use UHHiloLibrary\Circulation\ExitCount as ExitCount;
+
+require_once 'lib/start.php';
+define('VG_DB', 'VGER');
+define('VG_U', 'alluhidb');
+define('VG_P', 'alluhidb01');
+
+$DB = array(
+	"Voyager" => new Voyager(),
+	"Status" => new Status(),
+	"Study Room" => new StudyRoom(),
+	"Internal" => new InternalCount(),
+	"Exit" => new ExitCount(),
+	"Information" => new Information()
+);
+
+$item = 80000046370;
+$item = 80000123456;
+$date = '10/01/2016';
+
+echo "<ol>";
+foreach ($DB as $key => $value) {
+	echo "<li>{$key}: {$value->check_connection()}</li>";
+}
+echo "</ol>";
+
+var_dump($DB['Voyager']->get(80000123456));
+
+var_dump($DB['Information']->getInfo($item));
+
+?>
